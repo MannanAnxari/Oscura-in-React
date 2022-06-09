cb9ac9e2c68039b4d3b876e3de59f41e95d43450
Mannan <mannananxari@gmail.com>
2022-06-09 19:24:49 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,24 @@ const port = 45 || process.env.PORT;
 const app = express();
 const server  = http.createServer(app);
 const io = socketIO(server);
-
+var count = 0;
+const groupUsers = [{}];
 
 app.use(cors());
 app.use("/", (req, res) => {
     res.send("hello");
 })
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
     console.log("new Connection");
+    count++;
+    socket.on("joined", ({user}) => {
+        groupUsers[socket.id] = user;
+        console.log(`${user} has Joined`);
+        // console.log(groupUsers);
+    })
+    socket.emit('send', {user:"Admin", message: "Welcome To The Chat"});
+    socket.broadcast.emit("UserJoined", {user: "Admin", message: "User Has Joined"})
 })
 
 server.listen(port, () => {
