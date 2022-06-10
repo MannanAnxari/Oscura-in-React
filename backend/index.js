baa37d110d987165562114079920b97394b27643
Mannan <mannananxari@gmail.com>
2022-06-10 08:14:57 +0500

    FnF Almost

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const socketIO = require("socket.io");
 
 const port = 45 || process.env.PORT;
 const app = express();
-const server  = http.createServer(app);
+const server = http.createServer(app);
 const io = socketIO(server);
 var count = 0;
 const groupUsers = [{}];
@@ -17,16 +17,27 @@ app.use("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log("new Connection");
-    count++;
-    socket.on("joined", ({user}) => {
+    socket.on("joined", ({ user }) => {
+        count++;
         groupUsers[socket.id] = user;
-        console.log(`${user} has Joined`);
-        // console.log(groupUsers);
+        // console.log(`${user} has Joined`);
+        socket.broadcast.emit("UserJoined", { user: "Admin", message: `${groupUsers[socket.id]} Has Joined` })
+        socket.emit('send', { user: "Admin", message: `Welcome To The Chat, ${groupUsers[socket.id]}` });
+        socket.emit('TotalNo', { totalUsers: `${count}` })
+    });
+
+    socket.on('message', ({message, id}) => {
+        io.emit('sendMsg', {user: groupUsers[id],  message, id})
     })
-    socket.emit('send', {user:"Admin", message: "Welcome To The Chat"});
-    socket.broadcast.emit("UserJoined", {user: "Admin", message: "User Has Joined"})
+
+    socket.on("disconnect", () => {
+        count--;
+        socket.broadcast.emit("leave", { user: "Admin", message: `${groupUsers[socket.id]} Has Left` })
+        console.log('User Left');
+        socket.emit('TotalNo', { totalUsers: `${count}` })
+    });
 })
 
 server.listen(port, () => {
     console.log(`app listening on port ${port}`)
-  })
+})
